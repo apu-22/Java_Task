26dd61f8e215d64045cf9c4a31093236e90a071c
Apu Rayhan <mdapurayhan1729@gmail.com>
2025-02-22 02:12:45 +0600

    Calculating the volume of Sphere

---
+++ src/Practice_Problem1/SphereVolume.java
@@ -0,0 +1,18 @@
+package Practice_Problem1;
+
+import java.util.Scanner;
+
+public class SphereVolume {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the radius of the sphere: ");
+        double radius = scanner.nextDouble();
+
+        double volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+
+        System.out.printf("The volume of the sphere is: %.2f\n", volume);
+
+        scanner.close();
+    }
+}
