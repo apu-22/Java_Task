73b95eeead9b14faa8785d3875399344219d9fc5
Apu Rayhan <mdapurayhan1729@gmail.com>
2025-02-22 03:13:52 +0600

    Check the given year is leap-year or not

---
+++ src/Practice_Problem2/Leap_Year.java
@@ -0,0 +1,17 @@
+package Practice_Problem2;
+
+import java.util.Scanner;
+public class Leap_Year {
+    public static void main(String [] args){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the year: ");
+        int yr= scan.nextInt();
+
+        if((yr % 400==0) || (yr%4==0 && yr%100 !=0))
+            System.out.println(yr + " is lear year.");
+        else
+            System.out.println(yr + " is not leap year.");
+
+        scan.close();
+    }
+}
